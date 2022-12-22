253ddad1bfae2c85e05b82ebf29960fc3edcaa3e
root <root@0436c5516b86>
2022-12-22 03:57:42 +0000

    finished Nasa Countdown continue next

--- src/_04_spot_the_error/MethodCall.java
+++ src/_04_spot_the_error/MethodCall.java
@@ -7,10 +7,26 @@ public class MethodCall {
 	// Add ONE line of code to fix this program.
 	
 	public static void main(String[] args) {
-		
+		printMessage("hello World");
 	}
 	
+	
+
 	static void printMessage(String aMessage) {
 		System.out.println(aMessage);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/_99_extra/NasaCountdown.java
+++ src/_99_extra/NasaCountdown.java
@@ -1,6 +1,8 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 /* NASA wants to shoot a rocket into orbit.
  * Somebody has to do the countdown or the rocket won't launch.
  * 
@@ -13,17 +15,41 @@ package _99_extra;
 public class NasaCountdown {
     public static void main(String[] args) throws InterruptedException {
         // 2. Use a pop-up to ask the user where to start counting from
-        
+  String count = JOptionPane.showInputDialog(null, "Where do you want start counting from");
         // 3. Change the countdown to use the new starting point
         
+  
+  int countdown = Integer.parseInt(count);
         // 1. Print a countdown from 10 to 0 on the console
-            
-            // 4. Use the Sound.speak method to hear the countdown.
+    int Fo = -1 ;    
+       
+        for(int i =countdown; countdown >= 0; countdown--) {
+
+        	JOptionPane.showMessageDialog(null, countdown);
+      
+        
+   if(countdown==0)   
+   {
+	   JOptionPane.showMessageDialog(null, "blastoff!");
+   }
+          
+        	// 4. Use the Sound.speak method to hear the countdown.
             
             // 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
+            Thread.sleep(1000);
+        }      
             
         // 5. when the counting is done, speak "blastoff!"
-    }
+    speak("blastoff");
+        }
+    
+
+static void speak(String words) {
+	try {
+		Runtime.getRuntime().exec("say " + words).waitFor();
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+	}
 }
-
-
+}
