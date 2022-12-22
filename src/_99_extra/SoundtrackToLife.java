00e8201086cf5eb14fe8e1f5477916493e790aa2
root <root@0436c5516b86>
2022-12-22 04:00:57 +0000

    continue soundtrack to life

--- src/_99_extra/SoundtrackToLife.java
+++ src/_99_extra/SoundtrackToLife.java
@@ -11,7 +11,7 @@ public class SoundtrackToLife {
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
-		
+		f
 		// 2. Their answer is stored in the userMood variable. Print it out.
 
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
@@ -37,6 +37,11 @@ public class SoundtrackToLife {
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
+	if(ewe)
+	{
+		oijweofjoiwjeofjaoiweofjaweif
+	}
+		
 		}
 	}
 }
